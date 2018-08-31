ce03d2d8f0dd108e26d4a1f06a0052ea327b119b
Guillaume Ah-ki <guillaumeahki@gmail.com>
2018-08-31 11:34:36 -0400

    initialize

---
+++ README.md
@@ -0,0 +1,51 @@
+# React-back-to-top-button
+
+A React higher-order component that scrolls back to the top.
+
+## Installation
+
+With npm:
+
+```sh
+npm install react-back-to-top-button
+```
+
+With yarn:
+
+```sh
+yarn add react-back-to-top-button
+```
+
+## Example
+
+```jsx
+import React, { Component } from "react";
+import BackToTop from "react-back-to-top";
+
+class App extends Component {
+  render() {
+    return (
+      <BackToTop
+        showOnScrollUp
+        showAt={100}
+        speed={1500}
+        easing="easeInOutQuint"
+      >
+        UP
+      </BackToTop>
+    );
+  }
+}
+```
+
+## Props
+
+| Prop           | Required | Default         | Type                            | Description                                                                       |
+| -------------- | -------- | --------------- | ------------------------------- | --------------------------------------------------------------------------------- |
+| children       |          | `"UP"`          | `React.ReactElement` / `string` | content of the back to top component                                              |
+| scrollTo       |          | `0`             | `number`                        | scroll a certain position on click                                                |
+| showOnScrollUp |          | `false`         | `boolean`                       | show the back to top button only when the user scrolls up.                        |
+| showAt         |          | `1000`          | `number`                        | show the button at y position                                                     |
+| speed          |          | `2000`          | `number`                        | scrolling speed                                                                   |
+| easing         |          | `"easeOutSine"` | `string`                        | scroll timing function. Options: "easeOutSine", "easeInOutSine", "easeInOutQuint" |
+| style          |          |                 | `Object`                        | style of the floating button.                                                     |
---
+++ src/index.js
@@ -0,0 +1,134 @@
+import React, { PureComponent } from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  font-size: 45px;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  margin: 30px;
+  border-radius: 50%;
+  border: none;
+  opacity: 0;
+  transition: 0.4s opacity ease;
+  cursor: pointer;
+  outline: none;
+`;
+
+function scrollToY(scrollTargetYY, speedd, easingg) {
+  const scrollY = window.scrollY || document.documentElement.scrollTop;
+  const scrollTargetY = scrollTargetYY || 0;
+  const speed = speedd || 2000;
+  const easing = easingg || "easeOutSine";
+  let currentTime = 0;
+
+  const time = Math.max(
+    0.1,
+    Math.min(Math.abs(scrollY - scrollTargetY) / speed, 0.8)
+  );
+
+  const easingEquations = {
+    easeOutSine: pos => Math.sin(pos * (Math.PI / 2)),
+    easeInOutSine: pos => -0.5 * (Math.cos(Math.PI * pos) - 1),
+    easeInOutQuint: pos => {
+      let newPos = pos;
+      newPos /= 0.5;
+      if (newPos < 1) {
+        return 0.5 * newPos ** 5;
+      }
+      return 0.5 * (newPos - 2) ** 5 + 2;
+    }
+  };
+
+  function tick() {
+    currentTime += 1 / 60;
+
+    const p = currentTime / time;
+    const t = easingEquations[easing](p);
+
+    if (p < 1) {
+      requestAnimFrame(tick);
+
+      window.scrollTo(0, scrollY + (scrollTargetY - scrollY) * t);
+    } else {
+      window.scrollTo(0, scrollTargetY);
+    }
+  }
+
+  tick();
+}
+
+class BackToTop extends PureComponent {
+  state = {
+    isAtRange: false
+  };
+
+  componentDidMount = () => {
+    window.requestAnimFrame = (() => {
+      const clb = callback => {
+        window.setTimeout(callback, 1000 / 60);
+      };
+
+      return (
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        clb
+      );
+    })();
+
+    let lastScrollTop = 0;
+
+    window.addEventListener(
+      "scroll",
+      () => {
+        const st = window.pageYOffset || document.documentElement.scrollTop;
+
+        const backToTop = document.querySelector(".back-to-top");
+
+        const showAt = this.props.showAt || 1000;
+
+        if (st > showAt) {
+          this.setState({ isAtRange: true });
+        } else {
+          this.setState({ isAtRange: false });
+        }
+
+        if (this.props.showOnScrollUp) {
+          if (st > lastScrollTop) {
+            backToTop.style.opacity = 0;
+          } else if (this.state.isAtRange) {
+            backToTop.style.opacity = 1;
+          } else {
+            backToTop.style.opacity = 0;
+          }
+        } else if (this.state.isAtRange) {
+          backToTop.style.opacity = 1;
+        } else {
+          backToTop.style.opacity = 0;
+        }
+
+        lastScrollTop = st <= 0 ? 0 : st;
+      },
+      false
+    );
+  };
+
+  moveToTop = () => {
+    scrollToY(this.props.scrollTo, this.props.speed, this.props.easing);
+  };
+
+  render() {
+    return (
+      <Button
+        className="back-to-top"
+        onClick={this.moveToTop}
+        style={this.props.style}
+      >
+        {this.props.children || "UP"}
+      </Button>
+    );
+  }
+}
+
+export default BackToTop;
